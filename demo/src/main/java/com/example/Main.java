d5bdf2ceda979fc84ad2b606d786b11525008f48
NonSumJaponicus <sssffjoshua@gmail.com>
2024-09-04 09:54:05 +0700

    test: added example files

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(1+2);
+
+        int length = 5;
+        int width = 3;
+
+        System.out.println("Rectangle area: " + length * width);
+
+        double radius = 5;
+
+        System.out.println("Circle diameter: " + 2 * radius);
+        System.out.println("Circle area: " + radius * radius * 3.14);
+        System.out.println("Circle circumference: " + 2 * radius * 3.14);
+
+        int angleA = 80;
+        int angleB = 65;
+        System.out.println("angleC = " + (180 - angleA - angleB));
+
+        LocalDate date1 = LocalDate.of(2024, 3, 19);
+        LocalDate date2 = LocalDate.of(2024, 3, 21);
+        System.out.println(ChronoUnit.DAYS.between(date1, date2));
+
+        String firstName = "John";
+        String lastName = "Doe";
+        String initials = firstName.substring(0, 1) + lastName.substring(0, 1);
+        System.out.println(initials);
+    }
+}
