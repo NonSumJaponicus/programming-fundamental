efde99122f57c792d3ec7e013f5031637b4a9000
NonSumJaponicus <sssffjoshua@gmail.com>
2024-09-04 10:34:29 +0700

    feat: added comments

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -19,14 +19,18 @@ public class Main {
         System.out.println("Circle area: " + radius * radius * 3.14);
         System.out.println("Circle circumference: " + 2 * radius * 3.14);
 
+
+        // Print remaining angle of a triangle
         int angleA = 80;
         int angleB = 65;
         System.out.println("angleC = " + (180 - angleA - angleB));
 
+        // Print difference between dates
         LocalDate date1 = LocalDate.of(2024, 3, 19);
         LocalDate date2 = LocalDate.of(2024, 3, 21);
         System.out.println(ChronoUnit.DAYS.between(date1, date2));
 
+        // Initials
         String firstName = "John";
         String lastName = "Doe";
         String initials = firstName.substring(0, 1) + lastName.substring(0, 1);
