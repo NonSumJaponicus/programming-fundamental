4d599bc0635a3061489215b40c6456bd14ac206f
NonSumJaponicus <sssffjoshua@gmail.com>
2024-09-05 09:28:19 +0700

    "feat: day 3 update"

--- module_1_day_3/module1Day3/src/App.java
+++ module_1_day_3/module1Day3/src/App.java
@@ -45,6 +45,22 @@ public class App {
         return maxNumber;
     }
 
+    public static int getSmallestElement(int[] numberArray) {
+        // Get largest element in numberArray
+        int minNumber = 0;
+        
+        for (int i = 0; i < numberArray.length; i++) {
+            // Stores the current number in minNumber for comparison
+            if (i == 0)
+                minNumber = numberArray[i];
+            
+            if (numberArray[i] < minNumber)
+                minNumber = numberArray[i];
+        }
+
+        return minNumber;
+    }
+
     /*
     public static int calculateNumbers(String expression) {
         // Simple calculator for 2 numbers
@@ -90,6 +106,75 @@ public class App {
         return Arrays.toString(sequence);
     }
 
+    public static int countVowels(String word) {
+        word = word.toLowerCase();
+        int vowelCount = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == 'a' ||
+                word.charAt(i) == 'i' ||
+                word.charAt(i) == 'u' ||
+                word.charAt(i) == 'e' ||
+                word.charAt(i) == 'o') {
+                    vowelCount++;
+                }
+        }
+
+        return vowelCount;
+    }
+
+    public static String sortElements(int[] numberArray) {
+        int arrayLength = numberArray.length;
+        int[] returnArray = new int[arrayLength];
+
+        for (int i = 0; i < arrayLength; i++) {
+            int[] arrayCopy = Arrays.copyOfRange(numberArray, i, arrayLength);
+            returnArray[i] = getSmallestElement(arrayCopy);
+        }
+
+        return Arrays.toString(returnArray);
+    }
+
+    public static boolean checkIfAnagram(String word1, String word2) {
+       word1 = word1.toLowerCase();
+       word2 = word2.toLowerCase();
+        
+        int word1ASCIIValue = 0;
+        int word2ASCIIValue = 0;
+        
+        if (word1.length() == word2.length()) {
+            for (int i = 0; i < word1.length(); i++) {
+                word1ASCIIValue += (int) word1.charAt(i);
+            }
+            for (int j = 0; j < word2.length(); j++) {
+                word2ASCIIValue += (int) word2.charAt(j);
+            }
+
+        if (word1ASCIIValue == word2ASCIIValue)
+            return true;
+        }
+        return false;
+    }
+
+    public static int findSecondSmallestElement (int[] numberArray) {
+        int smallestNumber = 0;
+        int secondSmallestNumber = 0;
+
+        smallestNumber = numberArray[0];
+        secondSmallestNumber = numberArray[0];
+
+        for (int i = 1; i < numberArray.length; i++) {
+            if (numberArray[i] < smallestNumber) {
+                smallestNumber = numberArray[i];
+            }
+            else if (numberArray[i] > smallestNumber && numberArray[i] < secondSmallestNumber) {
+                secondSmallestNumber = numberArray[i];
+            }
+        }
+
+        return secondSmallestNumber;
+    }
+
     public static void main(String[] args) {
         System.out.println(sumDigits("1234"));
 
@@ -103,5 +188,14 @@ public class App {
 
         System.out.println(generateFibonacci(6));
 
+        System.out.println(countVowels("programming"));
+
+        int[] numberArray2 = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println(sortElements(numberArray2));
+
+        System.out.println(checkIfAnagram("listen", "silent"));
+
+        int[] numberArray3 = {5, 3, 8, 1, 2, 9};
+        System.out.println(findSecondSmallestElement(numberArray3));
     }
 }
